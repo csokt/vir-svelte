216a23a2c1eec290ffa403bfa7cf1d1e94d204ec
Tibor <csokt0@gmail.com>
2020-11-16 16:25:39 +0100

    Initial commit

--- tir/src/config/cards/norma.js
+++ tir/src/config/cards/norma.js
@@ -6,25 +6,28 @@ let startDate = new Date()
 startDate.setHours(5,30,0,0)
 
 function onChange(fields) {
-  fields.aktIdo.value = new Date().toLocaleTimeString('hu', {timeStyle: 'short'})
-  fields.elteltPerc.value = Math.round((new Date().getTime() - fields.kezdIdo.value.getTime())/60000)
+  let aktIdo = new Date()
+  aktIdo.setSeconds(0,0)
+  const elteltPerc = Math.round((aktIdo.getTime() - fields.kezdIdo.value.getTime())/60000)
 
+  fields.aktIdo.value = aktIdo.toLocaleTimeString('hu', {timeStyle: 'short'})
+  fields.elteltPerc.value = elteltPerc
+  fields.teljesitmeny.value = Math.round(100 * fields.osszesNormaperc.value / elteltPerc)
 }
 
 export default {
   id: 'norma',
   name: '',
   onMount: async (fields) => {
-      // SELECT sum([Összes Normaperc]) AS sum FROM monitor_napikodolas where [Dolgozó kód] = ${data.user.belepokod}
     const sql = `
-      SELECT sum([Összes Normaperc]) AS sum FROM monitor_napikodolas where [Dolgozó kód] = 20007
+      SELECT sum([Összes Normaperc]) AS sum FROM monitor_napikodolas where [Dolgozó kód] = ${data.user.belepokod}
     `
     const result = await api.post({url: '/local/tir/query', expect: 'object', params: {sql: sql}})
-    console.log(result)
-    // onChange(fields)
+    fields.osszesNormaperc.value = result.sum ? Math.round(result.sum) : 0
+    onChange(fields)
   },
   elements: [
-    common.alert_fields_button,
+    // common.alert_fields_button,
     {
       id: 'kezdIdo',
       name: 'Munkaidő kezdete',
@@ -50,7 +53,7 @@ export default {
       id: 'osszesNormaperc',
       name: 'Összes normaperc',
       type: 'text',
-      value: '',
+      value: 0,
       attributes: {readonly: true}
     },
     {
